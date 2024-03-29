92c70d982f91ebd516f9807fc11c4c47800f00e0
Shreyas Puttaraju <shreyas2797@gmail.com>
2024-03-28 20:35:29 -0500

    modified the prompts

--- lambda_function.py
+++ lambda_function.py
@@ -14,25 +14,10 @@ import os
 
 s3_client = boto3.client('s3')
 
-# Set up the model
-generation_config = {
-    "temperature": 0.2,
-    "top_p": 1,
-    "top_k": 1,
- }
-
-safety_settings = [
-    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-]
 
 # Initialize your genai model
 model = genai.GenerativeModel(
-    model_name="gemini-pro",
-    generation_config=generation_config,
-    safety_settings=safety_settings,
+    model_name="gemini-pro"
 )
 
 # Configure API key
